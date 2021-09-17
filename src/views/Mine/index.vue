93ae54db10ae7f6fbc5da0127aab0deb96f69c0e
hyh-op <1564894152@qq.com>
2021-09-17 20:53:23 +0800

    add createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main">
+        <Header title="hyh中心"></Header>
+        	<div id="content">
+                <Login></Login>
+            </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from  '@/components/Header'
+import TabBar from  '@/components/TabBar'
+import Login from '@/components/Login'
+export default {
+        name : 'Movie',
+        components:{
+            Header,
+            TabBar,
+            Login
+        }
+}
+</script>
+
+<style lang="scss" scoped>
+#content .login_body{  width:100%;}
+</style>
